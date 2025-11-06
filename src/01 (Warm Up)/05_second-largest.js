da94aed3843a5f9598facf0d3cf5d0ae0f4b3bc1
Abhishek Maradiya <mardiyaabhishek@gmail.com>
2025-11-06 21:08:31 +0530

    handle findSecondLargest edge cases

--- src/01 (Warm Up)/05_second-largest.js
+++ src/01 (Warm Up)/05_second-largest.js
@@ -2,15 +2,23 @@
 
 // Given an array, find and return the second largest value in the array.
 
+// Input: arr = [4, 9, 12, 0, 12, 10, 8, 7, 1];
+// Output: 10
+
 const findSecondLargest = (arr) => {
 	let firstLargest = -Infinity;
 	let secondLargest = -Infinity;
+
+	if (arr.length < 2) {
+		return "Array should have atleast 2 elements";
+	}
 
 	for (let i = 0; i < arr.length; i++) {
 		if (arr[i] > firstLargest) {
 			secondLargest = firstLargest;
 			firstLargest = arr[i];
-		} else if (arr[i] > secondLargest) {
+			// arr[i] !== firstLargest --> Handle duplicate elements - without this check, result would be 12
+		} else if (arr[i] > secondLargest && arr[i] !== firstLargest) {
 			secondLargest = arr[i];
 		}
 	}
@@ -18,7 +26,7 @@ const findSecondLargest = (arr) => {
 	return secondLargest;
 };
 
-const arr = [4, 9, 0, 12, 10, 8, 7, 1];
+const arr = [4, 9, 12, 0, 12, 10, 8, 7, 1];
 
 const ans = findSecondLargest(arr);
 console.log("ans:", ans);
