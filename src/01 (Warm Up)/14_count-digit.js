cce1abb6b5dc89c909955e1021ef85345b4e7600
Abhishek Maradiya <mardiyaabhishek@gmail.com>
2025-11-10 22:27:31 +0530

    update count digit problem to include handling of decimal numbers

--- src/01 (Warm Up)/14_count-digit.js
+++ src/01 (Warm Up)/14_count-digit.js
@@ -11,6 +11,7 @@ const countDigit = (num) => {
 	// Math.abs(-123) → 123
 	// Math.abs(123) → 123
 	// Math.abs(0) → 0
+	// Math.abs(11.5) → 11.5
 	num = Math.abs(num);
 
 	let count = 0;
@@ -18,7 +19,7 @@ const countDigit = (num) => {
 		// Math.floor() --> Rounds a number down to the nearest integer (removes the decimal part).
 		// Examples:
 		// Math.floor(7.8) → 7
-		// Math.floor(3.2) → 3
+		// Math.floor(7.2) → 7
 		// Math.floor(-4.7) → -5
 
 		num = Math.floor(num / 10);
