db272bd5affdf133f1554c4aefadf232ac723ac5
Abhishek Maradiya <mardiyaabhishek@gmail.com>
2025-11-16 11:33:59 +0530

    add reverse integer problem

--- src/01-warm-up/16_reverse-Integer.js
+++ src/01-warm-up/16_reverse-Integer.js
@@ -1,22 +1,43 @@
 // Reverse Integer
 
-// Given a signed 32-bit integer x, return x with its digits reversed. If reversing x causes the value to go outside the signed 32-bit integer range [-231, 231 - 1], then return 0.
+// Given a signed 32-bit integer x, return x with its digits reversed. If reversing x causes the value to go outside the signed 32-bit integer range [-2^31 , 2^31 - 1], then return 0.
 // LeetCode: https://leetcode.com/problems/reverse-integer/description/
 
-// Input: 123
-// Output: 321
+// Input: -123
+// Output: -321
 
 // Test Cases:
-// Input: -123 --> Output: -321
+// Input: 123 --> Output: 321
 // Input: 120 --> Output: 21
 
 // Constraints: -2^31 <= x <= 2^31 - 1
 
-const reverseInteger = (num) => {};
+const reverseInteger = (num) => {
+	const original = num;
 
-const num = 123;
+	if (num < 0) {
+		num = Math.abs(num);
+	}
+
+	let rev = 0;
+	while (num > 0) {
+		const rem = num % 10;
+		rev = rev * 10 + rem;
+		num = Math.floor(num / 10);
+	}
+
+	// Introduced in ES2016, the infix exponentiation operator ** is an alternative for the standard Math.pow function.
+	const constraint = 2 ** 31;
+	if (rev > constraint || rev < -constraint) {
+		return 0;
+	}
+
+	return original < 0 ? -rev : rev;
+};
+
+const num = -123;
 
 const ans = reverseInteger(num);
 console.log("ans:", ans);
 
-export { isPalindrome };
+export { reverseInteger };
